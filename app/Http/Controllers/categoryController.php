6202ad5515e36b63f7737c710c6856d2a5ec3791
aqillarossa <rossaaqila8@gmail.com>
2024-04-15 17:40:24 +0700

    nambahin edit di kategori lupa

--- app/Http/Controllers/categoryController.php
+++ app/Http/Controllers/categoryController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 use App\Models\category;
 use Illuminate\Http\RedirectResponse;
@@ -26,5 +26,22 @@ class categoryController extends Controller
 
         return redirect()->route('kategori.index')->with('success','Kategori Berhasil Ditambahkan.');
 }
+
+public function edit(Category $kategori): View
+    {
+        return view('categori.edit', compact('kategori'))->with(["title" => "Edit Kategori"]);
+    }
+
+    public function update(Request $request, Category $kategori):RedirectResponse
+    {
+        $request->validate([
+            "name" => "required",
+            "description" => "required",
+        ]);
+
+
+        $kategori->update($request->all());
+        return redirect()->route('kategori.index')->with('updated','Kategori Berhasil Diubah.');
+    }
 
 }
